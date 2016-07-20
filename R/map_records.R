81c8283bbd94762158424eb5289b21119d6dfb39
mainic <nicolas.maire@unibas.ch>
2016-07-20 09:02:12 +0200

    initial commit to xva repo

---
+++ R/map_records.R
@@ -0,0 +1,47 @@
+# wrapper around eval, with some extra functionality
+eval_expr <- function(expr) {
+  if (nchar(expr) == 0) {
+    return("")
+  }
+  value <- ""
+  value <- tryCatch(eval(parse(text = expr)))
+  return(value)
+}
+
+#' @export
+map_records <- function(records, mapping_file) {
+  mapping_f_name <- system.file('mapping', mapping_file, package = 'xva')
+  headers <- names(records)
+
+  # Load mapping tab-delim file:
+  mapping <- read.delim(mapping_f_name)
+
+  # number of variables required by coding algorithm
+  target_n <- nrow(mapping)
+  output_data <- data.frame(matrix(ncol = target_n))
+  colnames(output_data) <- mapping[, 1]
+  for (rec_count in 1:nrow(records)) {
+    assign("rec_id", rec_count, envir = .GlobalEnv)
+    record <- records[rec_count, ]
+    for (j in 1:length(headers)) {
+      value <- as.character(record[1, j])
+      header <- headers[j]
+      header_cleaned <-
+        regmatches(header, regexpr("[^\\.]*$", header))
+      assign(header_cleaned, value, envir = .GlobalEnv)
+    }
+    current_data <- data.frame(matrix(ncol = target_n))
+    for (i in 1:target_n) {
+      target_var <- as.character(mapping[i, 1])
+      expr <- as.character(mapping[i, 2])
+      current_data[i] <- eval_expr(expr)
+      # make the value available for reference later in the destination var set
+      name <- regmatches(target_var, regexpr("[^\\-]*$", target_var))
+      name <- paste("t_", name, sep = "")
+      assign(name, current_data[i][[1]], envir = .GlobalEnv)
+    }
+    output_data[rec_count, ] <- current_data
+  }
+  return(output_data)
+}
+
---
+++ R/utils.R
@@ -0,0 +1,88 @@
+# map true to character 'y'
+true_to_y <- function(expr) {
+  value <- eval(parse(text = expr))
+  if (value == TRUE) {
+    return("y")
+  } else {
+    return("")
+  }
+}
+
+multi_select_contains <- function(what, who_id) {
+  if (who_id == -1) {
+    return(FALSE)
+  }
+  split_expression <- as.list(strsplit(who_id, " ")[[1]])
+  found <- FALSE
+  for (selection in split_expression) {
+    if (grepl(what, selection)) {
+      found <- TRUE
+    }
+  }
+  return(found)
+}
+
+yes_to_code <- function(qlist, clist, default) {
+  code <- ""
+  for (i in 1:length(qlist)) {
+    if (get(qlist[i]) == "yes") {
+      code <- paste(code, clist[i])
+    }
+  }
+  # use default if code is empty
+  if (nchar(code) == 0) {
+    code <- default
+  }
+  return(trimws(code))
+}
+
+# from_list: upper limits of range to_list: codes to map to
+range_to_code <- function(from_list, to_list, default, who_id) {
+  if (get(who_id) == "") {
+    return("")
+  }
+  code <- default
+  value <- as.numeric(get(who_id))
+  for (i in 1:length(to_list)) {
+    if (value > from_list[i] && value <= from_list[i + 1]) {
+      code <- to_list[i]
+    }
+  }
+  return(code)
+}
+
+#map between sets of codes
+map_code <- function(from_list, to_list, who_id) {
+  code <- ""
+  value <- get(who_id)
+  for (i in 1:length(from_list)) {
+    if (from_list[i] == value) {
+      code <- to_list[i]
+    }
+  }
+  # leave empty if no match
+  return(code)
+}
+
+#Create ODK-style muli-select answer (chosen options listed separated by space)
+map_multi_code <- function(from_list, to_list, who_id) {
+  code <- ""
+  values <- strsplit(as.character(get(who_id)), " ")[[1]]
+  for (i in 1:length(from_list)) {
+    if (any(from_list[i] == values)) {
+      code <- paste(code, to_list[i])
+    }
+  }
+  # leave empty if no match
+  return(trimws(code))
+}
+
+# evaluate expression, return default on empty
+exp_def <- function(expr, default) {
+  value <- eval_expr(expr)
+  if (nchar(value) > 0) {
+    return(value)
+  } else {
+    return(default)
+  }
+}
